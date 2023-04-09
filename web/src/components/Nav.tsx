6aacc9c4ff31d5637fc4cd6c71739236f4faa10c
aking526 <asking52607@gmail.com>
2023-04-08 21:31:26 -0400

    update nav

--- web/src/components/Nav.tsx
+++ web/src/components/Nav.tsx
@@ -1,51 +1,73 @@
 import React, { MouseEventHandler, useState, useEffect } from "react";
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Stack, Flex } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import type Pair from "../utils/Pair";
 import NavLinkBtn from "./NavLinkBtn";
 
-interface NavProps {}
+interface NavProps {
+  routes: Pair<string, string>[];
+}
 
-const Nav: React.FC<NavProps> = ({ }) => {
-	const [show, setShow] = useState(false);
+const Nav: React.FC<NavProps> = ({ routes }) => {
+	const [isOpen, setIsOpen] = useState(false);
 
 	const location = useLocation();
 	useEffect(() => {
-		setShow(false)
+		setIsOpen(false)
 	}, [location]);
 
 	const handleClick: MouseEventHandler<HTMLDivElement> = (event) => {
 		event.preventDefault();
-		setShow(prevState => !prevState);
+		setIsOpen(prevState => !prevState);
 	};
+
+  const iconVariants = {
+    opened: {
+      rotate: 135,
+    },
+    closed: {
+      rotate: 0,
+    }
+  };
+
+  // @ts-ignore
+  const nameTitleWidth = document.getElementById("open-btn-name-title-container")?.offsetWidth - document.getElementById("open-btn")?.offsetWidth;
+  const topBarVariants = {
+    opened: {
+      x : 0    
+    },
+    closed: {
+      x: -264 // nameTitleWidth
+    }
+  };
 
 	return (
 		<Box position="absolute" zIndex={2} h="100vh">
-			<Box position="absolute" onClick={handleClick} _hover={{ cursor: "pointer" }}>
-				{ show ?
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-					     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-					     className="feather feather-x">
-						<line x1="18" y1="6" x2="6" y2="18"></line>
-						<line x1="6" y1="6" x2="18" y2="18"></line>
-					</svg>
-					:
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-					     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-					     className="feather feather-menu">
-						<line x1="3" y1="12" x2="21" y2="12"></line>
-						<line x1="3" y1="6" x2="21" y2="6"></line>
-						<line x1="3" y1="18" x2="21" y2="18"></line>
-					</svg>
-				}
-			</Box>
-			{ show &&
-				<Stack w="20vw" h="100%" border="2px" borderColor="black" pt="5vh">
-					<NavLinkBtn value="Main" linkTo="/" />
-					<NavLinkBtn value="Laws" linkTo="/laws" />
-					<NavLinkBtn value="About" linkTo="/about" />
-					<NavLinkBtn value="Contact" linkTo="/contact" />
-				</Stack>
-			}
+      <motion.div variants={topBarVariants} animate={isOpen ? "opened" : "closed"}>
+        <Flex id="open-btn-name-title-container" direction="row" justifyContent="space-between" width="20vw">
+          <Box id="name-title" as="h3">NYC Energy Maps</Box>
+  			  <Box id="open-btn" _hover={{ cursor: "pointer" }} p="0.5max">
+            <motion.div variants={iconVariants} animate={isOpen ? "opened" : "closed"} whileHover={{ scale: 1.4 }} onClick={handleClick}>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5C13 4.44772 12.5523 4 12 4Z"
+                  fill="#000000"
+                />
+              </svg>
+            </motion.div>
+			    </Box>
+        </Flex>
+      </motion.div>
+      <Box position="absolute" w="20vw">
+        { isOpen &&
+				  <Stack border="2px" height="100vh" borderColor="black" pt="5vh">
+            { routes.map((route: Pair<string, string>, key: number) => {
+              return <NavLinkBtn key={key} value={route.first} linkTo={route.second} />;
+            })}
+				  </Stack>
+			  }
+      </Box>
 		</Box>
 	);
 };
